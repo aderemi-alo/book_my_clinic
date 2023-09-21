5ae69e9eaebba6f5bbc988f0d5b84fdc28a672c2
FlutterBugs <aloaderemi1@gmail.com>
2023-09-21 19:24:49 +0100

    linked firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,14 @@ import 'package:book_my_clinic/app/app.dialogs.dart';
 import 'package:book_my_clinic/app/app.locator.dart';
 import 'package:book_my_clinic/app/app.router.dart';
 import 'package:stacked_services/stacked_services.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await setupLocator();
   setupDialogUi();
   setupBottomSheetUi();
