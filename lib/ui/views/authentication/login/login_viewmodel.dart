92372504a9a8a82b36f95afbc23d95d6ee265741
FlutterBugs <aloaderemi1@gmail.com>
2023-09-28 10:34:25 +0100

    Added error handling for roles

--- lib/ui/views/authentication/login/login_viewmodel.dart
+++ lib/ui/views/authentication/login/login_viewmodel.dart
@@ -4,10 +4,14 @@ import 'package:book_my_clinic/services/authentication_service.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:book_my_clinic/ui/common/widgets.dart';
+import 'package:book_my_clinic/services/patient_service.dart';
+import 'package:book_my_clinic/services/doctor_service.dart';
 
 class LoginViewModel extends FormViewModel {
   final _authenticationService = locator<AuthenticationService>();
   final _navigationService = locator<NavigationService>();
+  final _patientService = locator<PatientService>();
+  final _doctorService = locator<DoctorService>();
   String role = 'patient';
 
   void login(String email, String password) async {
@@ -15,9 +19,21 @@ class LoginViewModel extends FormViewModel {
 
     if (result.isSuccess) {
       if (role == "patient") {
-        _navigationService.replaceWithPatientDashboardView();
+        var uid = _authenticationService.getCurrentUser()!.uid;
+        result = await _patientService.getPatient(uid);
+        if (result.isSuccess) {
+          await _navigationService.replaceWithPatientDashboardView();
+        } else {
+          showError("This account is not a Patient.");
+        }
       } else {
-        _navigationService.replaceWithDoctorDashboardView();
+        var uid = _authenticationService.getCurrentUser()!.uid;
+        result = await _doctorService.getDoctor(uid);
+        if (result.isSuccess) {
+          await _navigationService.replaceWithDoctorDashboardView();
+        } else {
+          showError("This account is not a Doctor.");
+        }
       }
     } else {
       showError(result.error);
